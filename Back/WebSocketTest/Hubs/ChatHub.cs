47f578287e624207262f6c503cd21d833abac62d
Fernando <fernando@tsplus.com.br>
2025-02-19 12:07:04 -0300

    iniciando o repositorio.

---
+++ Back/WebSocketTest/Hubs/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using WebSocketTest.Models;
+
+namespace WebSocketTest.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinChat(UserConnection connection)
+        {
+            await Clients
+                .All
+                .SendAsync("ReceiveMessage", "admin", $"{connection.UserName} nas joined");
+        }
+
+        public async Task JoinSpecificChatRoom(UserConnection connection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
+            await Clients
+                .Group(connection.ChatRoom)
+                .SendAsync(
+                    "ReceiveMessage", "admin", $"{connection.UserName} nas joined {connection.ChatRoom}"
+                );
+        }
+    }
+}
---
+++ Back/WebSocketTest/Program.cs
@@ -0,0 +1,41 @@
+using WebSocketTest.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSignalR();
+builder.Services.AddSignalR();
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseCors("AllowAllOrigins");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHub<ChatHub>("/Chat");
+
+app.Run();
