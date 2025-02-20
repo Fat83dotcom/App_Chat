94020dc1a5c57b057da77e73b472a05ae6cc5ba5
Fernando <fernando@tsplus.com.br>
2025-02-19 22:11:30 -0300

    Crinado a task de mensagens e conectando com uma base da de dados na memoria

--- Back/WebSocketTest/Hubs/ChatHub.cs
+++ Back/WebSocketTest/Hubs/ChatHub.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using WebSocketTest.DataService;
 using WebSocketTest.Models;
 
 namespace WebSocketTest.Hubs
 {
-    public class ChatHub : Hub
+    public class ChatHub(SharedDB sharedDB) : Hub
     {
+        private readonly SharedDB _sharedDB = sharedDB;
         public async Task JoinChat(UserConnection connection)
         {
             await Clients
@@ -15,11 +17,22 @@ namespace WebSocketTest.Hubs
         public async Task JoinSpecificChatRoom(UserConnection connection)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
+            _sharedDB.Connections[Context.ConnectionId] = connection;
             await Clients
                 .Group(connection.ChatRoom)
                 .SendAsync(
                     "ReceiveMessage", "admin", $"{connection.UserName} nas joined {connection.ChatRoom}"
                 );
         }
+
+        public async Task SendMessage(string msg)
+        {
+            if (_sharedDB.Connections.TryGetValue(Context.ConnectionId, out UserConnection? connection))
+            {
+                await Clients.Group(connection.ChatRoom)
+                    .SendAsync("ReceiveSpecificMessage", connection.UserName, msg);
+            }
+        }
+
     }
 }
