a42d975b506667d3ebedd1e57568bffff73fabd3
Fernando <fernando@tsplus.com.br>
2025-02-23 10:29:47 -0300

    refactor: melhorias no componente MessageContainer
    - Adicionada importação do arquivo CSS 'ScrollableBox.css' para estilização.
    - Implementado uso de `useRef` e `useEffect` para focar automaticamente na última mensagem recebida.
    - Refatorada a estrutura do componente para utilizar classes CSS e melhorar a organização do código.
    - Adicionada lógica para atribuir `ref` apenas à última mensagem, permitindo foco automático.
    - Ajustada a exibição das mensagens para incluir um indicador visual ('->') entre o nome do usuário e a mensagem.
    - Removido o uso de tabelas para exibição das mensagens, substituído por uma estrutura mais semântica e acessível.

--- Front/WebSocket/src/Components/MessageContainer.jsx
+++ Front/WebSocket/src/Components/MessageContainer.jsx
@@ -1,23 +1,34 @@
 import P from 'prop-types'
+import './ScrollableBox.css';
+import { useEffect, useRef } from 'react';
 
-export const MessageContainer = ({messages}) => {
-    console.log(messages)
-    return (
-        <>
-        {
-            messages.map((msg, index) => 
-                <div  key={index}>
-                    <table>
-                        <tr>
-                            <td>{msg.msg} - {msg.userName}</td>
-                        </tr>
-                    </table>
-                </div>
-            )
+export const MessageContainer = ({ messages }) => {
+    const lastMessageRef = useRef(null);
+
+    useEffect(() => {
+        if (lastMessageRef.current) {
+            lastMessageRef.current.focus();
         }
-        </>
-    )
-}
+    }, [messages]);
+
+    return (
+        <div className="scrollable-box">
+            {messages.map((msg, index) => {
+                const isLastMessage = index === messages.length - 1;
+                return (
+                    <div className='message-container' key={index}>
+                        <p 
+                            ref={isLastMessage ? lastMessageRef : null}
+                            tabIndex={-1}
+                        >
+                            {msg.userName} -{'>'} {msg.msg}
+                        </p>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
 
 MessageContainer.propTypes = {
     messages: P.array
